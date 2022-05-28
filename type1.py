579293af8e0da5a32adfcecfd4d749f21b46a1e9
tsuchiya227 <63704190+tsuchiya227@users.noreply.github.com>
2022-05-28 17:31:44 +0900

    Add files via upload

---
+++ type1.py
@@ -0,0 +1,15 @@
+for num in range(1, 101):
+    string = ''
+
+    # ここから記述
+    if num % 15 == 0:
+        string = "FizzBuzz"
+    elif num % 3 == 0:
+        string = "Fizz"
+    elif num % 5 == 0:
+        string = "Buzz"
+    else:
+        string = num
+    # ここまで記述
+
+    print(string)
